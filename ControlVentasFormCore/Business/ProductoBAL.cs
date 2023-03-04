975bad4fbc403b9fdce8c01ce9ff4c211210ff51
Abraham Canul <abraham23.acc@gmail.com>
2023-03-04 03:23:36 -0600

    CAPAS

---
+++ ControlVentasFormCore/Business/ProductoBAL.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlVentasFormCore.Business
+{
+    /// <summary>
+    /// Definicion de clase que permite exponer metodos de negocio para los productos
+    /// </summary>
+    public class ProductoBAL
+    {
+        #region Variables Globales...
+        private Data.ProductoDAL ProductoDAL;
+        #endregion
+
+        #region Constructor...
+        public ProductoBAL()
+        {
+            ProductoDAL = new Data.ProductoDAL();
+        }
+        #endregion
+
+        #region Properties
+        public string ConnectionString
+        {
+            get { return ProductoDAL.ConnectionString; }
+            set { ProductoDAL.ConnectionString = value; }
+        }
+        #endregion
+
+        #region Methods
+        public Entity.ProductoInfo GetProducto(int Id)
+        {
+            if (Id == 0) throw new ArgumentException("No recibi el id del producto que desea obtener");
+            return ProductoDAL.GetEntityObject(Id);
+        }
+
+        /// <summary>
+        /// Metodo que devuelve TODOS los productos existentes
+        /// </summary>
+        /// <param name="Producto"></param>
+        /// <returns></returns>
+        public List<Entity.ProductoInfo> GetProductos()
+        {
+            return ProductoDAL.FindBy(new Entity.ProductoInfo());
+        }
+
+        public List<Entity.ProductoInfo> GetProductosPorId(int CategoriaId)
+        {
+            if (CategoriaId == 0) throw new Exception("No recibí el Id de la categoría de la que se desean obtener los productos");
+            return ProductoDAL.FindBy(new Entity.ProductoInfo() { CategoriaId = CategoriaId });
+        }
+
+        public List<Entity.ProductoInfo> FindBy(Entity.ProductoInfo ProductoInfo)
+        {
+            if (ProductoInfo == null) throw new ArgumentNullException("No recibi un objeto entidad Producto para aplicar el filtro");
+            return ProductoDAL.FindBy(ProductoInfo);
+        }
+
+        public int Save(Entity.ProductoInfo ProductoInfo)
+        {
+            if (ProductoInfo == null) throw new ArgumentNullException("No recibi un objeto entidad Producto para aplicar el filtro");
+            if (ProductoInfo.Id == 0)
+                return ProductoDAL.Insert(ProductoInfo);
+            else
+                return ProductoDAL.Update(ProductoInfo);
+        }
+
+        public bool Delete(int Id)
+        {
+            ProductoDAL.Delete(Id);
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ ControlVentasFormCore/Data/ProductoDAL.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlVentasFormCore.Data
+{
+    public class ProductoDAL
+    {
+        #region Global Variables
+        public const string TableName = "Productos";
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// String de conexion a la bd
+        /// </summary>
+        public string ConnectionString { get; set; }
+
+        #endregion
+
+        #region Methods 
+
+        /// <summary>
+        /// devuelve un objeto entidad del id solicitado
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public Entity.ProductoInfo GetEntityObject(int Id)
+        {
+            string SQL = $"select * from {TableName} where {Entity.ProductoInfo.FieldName.Id} = {Id}";
+            DataTable dt = Utilerias.SQLHelper.ExecuteDatatable(SQL, ConnectionString);
+
+            if (dt == null || dt.Rows.Count == 0) throw new Exception($"No pude obtener los datos del Id [{Id}]");
+
+            return GetEntityObject(dt.Rows[0]);
+        }
+
+        /// <summary>
+        /// Devulve un objeto entidad con los datos recibidos en el row
+        /// </summary>
+        /// <param name="Row"></param>
+        /// <returns></returns>
+        public Entity.ProductoInfo GetEntityObject(DataRow Row)
+        {
+            if (Row == null) throw new ArgumentNullException("No recibimos el datarow para obtener los datos");
+
+            Entity.ProductoInfo ProductoInfo = new Entity.ProductoInfo();
+            ProductoInfo.Id = Convert.ToInt32(Row[Entity.ProductoInfo.FieldName.Id]);
+            ProductoInfo.Nombre = Row[Entity.ProductoInfo.FieldName.Nombre].ToString();
+            ProductoInfo.Descripcion = Row[Entity.ProductoInfo.FieldName.Descripcion].ToString();
+            ProductoInfo.Precio = Convert.ToDecimal(Row[Entity.ProductoInfo.FieldName.Precio].ToString());
+            ProductoInfo.CategoriaId = Convert.ToInt32(Row[Entity.ProductoInfo.FieldName.CategoriaId]);
+
+            return ProductoInfo;
+        }
+
+        /// <summary>
+        /// inserta el objeto entidad especificado en la BD
+        /// </summary>
+        /// <param name="ProductoInfo"></param>
+        /// <returns></returns>
+        public int Insert(Entity.ProductoInfo ProductoInfo)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"insert into {TableName} (");
+            sb.Append(Entity.ProductoInfo.FieldName.Nombre);
+            sb.Append($", {Entity.ProductoInfo.FieldName.Descripcion}");
+            sb.Append($", {Entity.ProductoInfo.FieldName.Precio}");
+            sb.Append($", {Entity.ProductoInfo.FieldName.CategoriaId}");
+            sb.AppendLine(")");
+            sb.AppendLine("values(");
+            sb.Append($"'{ProductoInfo.Nombre}'");
+            sb.Append($",'{ProductoInfo.Descripcion}'");
+            sb.Append($",'{ProductoInfo.Precio}'");
+            sb.Append($",'{ProductoInfo.CategoriaId}'");
+            sb.AppendLine(")");
+            sb.AppendLine("select SCOPE_IDENTITY()");
+
+            object Id = Utilerias.SQLHelper.ExecuteScalar(sb.ToString(), ConnectionString);
+            if (Id == null) return 0;
+            return Convert.ToInt32(Id);
+        }
+
+        /// <summary>
+        /// actualiza el objetoe entidad especificad
+        /// </summary>
+        /// <param name="ProductosInfo"></param>
+        /// <returns></returns>
+        public int Update(Entity.ProductoInfo ProductoInfo)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"update {TableName} set");
+            sb.Append($"{Entity.ProductoInfo.FieldName.Nombre} = '{ProductoInfo.Nombre}'");
+            sb.Append($",{Entity.ProductoInfo.FieldName.Descripcion} = '{ProductoInfo.Descripcion}'");
+            sb.Append($"{Entity.ProductoInfo.FieldName.Precio} = '{ProductoInfo.Precio}'");
+            sb.Append($"{Entity.ProductoInfo.FieldName.CategoriaId} = '{ProductoInfo.CategoriaId}'");
+            sb.Append($" where {Entity.ProductoInfo.FieldName.Id} = {ProductoInfo.Id}");
+
+            Utilerias.SQLHelper.ExecuteNonQuery(sb.ToString(), ConnectionString);
+            return ProductoInfo.Id;
+        }
+
+        /// <summary>
+        /// borra el objeto entidad con el id especificado
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public int Delete(int Id)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"delete {TableName}");
+            sb.Append($" where {Entity.ProductoInfo.FieldName.Id} = {Id}");
+
+            Utilerias.SQLHelper.ExecuteNonQuery(sb.ToString(), ConnectionString);
+            return Id;
+        }
+
+        /// <summary>
+        /// Devulve una lista de objetos entidad que cumplen las condiciones modificadas en el objeto entidad especificado
+        /// </summary>
+        /// <param name="Producto"></param>
+        /// <returns></returns>
+        public List<Entity.ProductoInfo> FindBy(Entity.ProductoInfo Producto)
+        {
+            string Filter = string.Empty;
+
+            //generamos un nuevo objeto para poder comparar y detectar variaciones
+            Entity.ProductoInfo nProducto = new Entity.ProductoInfo();
+            if (string.Compare(Producto.Nombre, nProducto.Nombre, true) != 0)
+                Filter += $" and {Entity.ProductoInfo.FieldName.Nombre} = '{Producto.Nombre}'";
+            if (string.Compare(Producto.Descripcion, nProducto.Descripcion, true) != 0)
+                Filter += $" and {Entity.ProductoInfo.FieldName.Descripcion} = '{Producto.Descripcion}'";
+            if (Producto.Precio == nProducto.Precio)
+                Filter += $" and {Entity.ProductoInfo.FieldName.Precio} = '{Producto.Precio}'";
+            if (Producto.CategoriaId == nProducto.CategoriaId)
+                Filter += $" and {Entity.ProductoInfo.FieldName.CategoriaId} = '{Producto.CategoriaId}'";
+
+            // generamos una sentencia sql con el filtro dinamico...
+            string sql = $"select * from {TableName} where {Entity.ProductoInfo.FieldName.Id} > 0 " + Filter;
+
+            DataTable dt = Utilerias.SQLHelper.ExecuteDatatable(sql, ConnectionString);
+
+            if (dt == null || dt.Rows.Count == 0) return null;
+
+            //generamos un ciclo por cada registro que devolvio la consulta y llenamos la lista
+            List<Entity.ProductoInfo> ProductosInfo = new List<Entity.ProductoInfo>();
+            foreach (DataRow Row in dt.Rows)
+                ProductosInfo.Add(GetEntityObject(Row));
+
+            return ProductosInfo;
+        }
+        #endregion
+    }
+}
