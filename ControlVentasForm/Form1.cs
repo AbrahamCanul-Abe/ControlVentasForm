63f256b32f72af4694b38d6c2375749257aa35eb
Abraham Canul <abraham23.acc@gmail.com>
2023-03-04 03:42:33 -0600

    form

--- ControlVentasForm/Form1.cs
+++ ControlVentasForm/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using ControlVentasFormCore;
 
 namespace ControlVentasForm
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+               
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                Cursor = Cursors.Default;
+            }
+
         }
     }
 }
